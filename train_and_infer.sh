9201193f5efe6783fbff2fa7f0b5f3b5ed3f9409
dragongeng <3349495429@qq.com>
2025-08-24 03:16:48 +0800

    进行自我介绍的sft

--- train_and_infer.sh
+++ train_and_infer.sh
@@ -44,10 +44,10 @@ num_nodes=1
 job_id=2023
 
 train_config=conf/ct_config.yaml
-gxl_data_json_info_path_s2t=conf/data_s2t.yaml
-gxl_data_json_info_path_t2s=conf/data_t2s.yaml
-gxl_data_json_info_path_s2s=conf/data_s2s.yaml
-gxl_data_json_info_path_t2t=conf/data_t2t.yaml
+gxl_data_json_info_path_s2t=conf/empty.yaml
+gxl_data_json_info_path_t2s=conf/empty.yaml
+gxl_data_json_info_path_s2s=conf/data_s2s_tmp.yaml
+gxl_data_json_info_path_t2t=conf/empty.yaml
 
 # 自然语言think的训练数据
 # epoch0 是 使用kokoro数据训练的，在内容层面先用kokoro数据训练出效果，先不管音质。
@@ -63,7 +63,7 @@ gxl_data_json_info_path_t2t=conf/data_t2t.yaml
 
 
 # dir=$exp_path/qwen2_multi_task_4_6gpus_gxl_adapter/epoch_12_13_with_speech_gxl_with_asr-chat
-dir=/home/A02_tmpdata2/ckpt/osum_chat_new_start_0810/epoch5_s2t_t2s_t2t_s2s_hq_language_think
+dir=/home/A02_tmpdata2/ckpt/osum_chat_new_start_0810/epoch5_s2t_t2s_t2t_s2s_hq_language_think_sft
 #checkpoint=/home/A02_tmpdata3/ckpt/osum_chat/epoch0_all_data/step_10624.pt
 #checkpoint=/home/A02_tmpdata3/ckpt/osum_chat/epoch0_all_data/step_14374.pt
 #checkpoint=/home/A02_tmpdata3/ckpt/osum_chat/epoch1_all_data/step_2816.pt
@@ -77,7 +77,8 @@ dir=/home/A02_tmpdata2/ckpt/osum_chat_new_start_0810/epoch5_s2t_t2s_t2t_s2s_hq_l
 #checkpoint=/home/A02_tmpdata2/ckpt/osum_chat_new_start_0810/epoch2_s2t_t2s_t2t_s2s_hq_language_think/step_8749.pt
 #checkpoint=/home/A02_tmpdata2/ckpt/osum_chat_new_start_0810/epoch3_s2t_t2s_t2t_s2s_hq_language_think/step_27499.pt
 #checkpoint=/home/A02_tmpdata2/ckpt/osum_chat_new_start_0810/epoch4_s2t_t2s_t2t_s2s_hq_language_think/step_23749.pt
-checkpoint=/home/A02_tmpdata2/ckpt/osum_chat_new_start_0810/epoch4_s2t_t2s_t2t_s2s_hq_language_think_new/step_49999.pt
+#checkpoint=/home/A02_tmpdata2/ckpt/osum_chat_new_start_0810/epoch4_s2t_t2s_t2t_s2s_hq_language_think_new/step_49999.pt
+checkpoint=/home/A02_tmpdata2/ckpt/osum_chat_new_start_0810/epoch5_s2t_t2s_t2t_s2s_hq_language_think/step_13749.pt
 
 mkdir -p $dir
 data=$dir/data
