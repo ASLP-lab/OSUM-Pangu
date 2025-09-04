b785d7eada8b5de6ec81edd3678d44d208d67dc2
dragongeng <3349495429@qq.com>
2025-09-04 21:53:07 +0800

    加入emotion raw data 继续训练

--- train_and_infer.sh
+++ train_and_infer.sh
@@ -70,7 +70,7 @@ gxl_data_json_info_path_t2t=conf/data_t2t.yaml
 
 
 # dir=$exp_path/qwen2_multi_task_4_6gpus_gxl_adapter/epoch_12_13_with_speech_gxl_with_asr-chat
-dir=/home/A02_tmpdata1/ckpt/osum_chat_new_start_0810/epoch6_add_emotion_raw_in_no_think
+dir=/home/A02_tmpdata1/ckpt/osum_chat_new_start_0810/epoch6_add_emotion_raw_in_no_think_hq_language_think
 #checkpoint=/home/A02_tmpdata3/ckpt/osum_chat/epoch0_all_data/step_10624.pt
 #checkpoint=/home/A02_tmpdata3/ckpt/osum_chat/epoch0_all_data/step_14374.pt
 #checkpoint=/home/A02_tmpdata3/ckpt/osum_chat/epoch1_all_data/step_2816.pt
