bc2158867fcb6700f9cfcbe19238e3cf34753ea3
xuelonggeng <3349495429@qq.com>
2025-09-17 12:03:24 +0800

    context asr trian epoch2

--- train_and_infer_contextasr.sh
+++ train_and_infer_contextasr.sh
@@ -70,7 +70,7 @@ gxl_data_json_info_path_t2t=conf/data_t2t.yaml
 
 
 # dir=$exp_path/qwen2_multi_task_4_6gpus_gxl_adapter/epoch_12_13_with_speech_gxl_with_asr-chat
-dir=/home/A02_tmpdata1/ckpt/context_asr_full/epoch0_all_task_test2_only_contextdata # 第一遍忘记加入think内容了
+dir=/home/A02_tmpdata1/ckpt/context_asr_full/epoch0_all_task_test2_only_contextdata 
 #checkpoint=/home/A02_tmpdata3/ckpt/osum_chat/epoch0_all_data/step_10624.pt
 #checkpoint=/home/A02_tmpdata3/ckpt/osum_chat/epoch0_all_data/step_14374.pt
 #checkpoint=/home/A02_tmpdata3/ckpt/osum_chat/epoch1_all_data/step_2816.pt
